be7a456d10e8f89e2da0b3dd3734fd5b6b3d7fd3
Riyad El Azhari <relaz051@uottawa.ca>
2020-10-17 19:12:20 -0400

    completed

--- MatchingClass.java
+++ MatchingClass.java
@@ -1,24 +1,32 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 public class MatchingClass{
+  //initializing all variables required
+  //first two arrays store only indices
     private static int[] employersindices;
     private static int[] studentsindices;
+    //these two arrays store names instead
     private static String[] employers;
     private static String[] students;
     private static List<HeapPriorityQueue<Integer,Integer>> PQ;
     private static int[][] A;
     private static Stack<Integer> Sue;
     private static int N;
-    public static void main(String[] args) {
-
+    //set of stable matches
+    private static HashMap<Integer, Integer> map;
+    public static void main(String[] args)throws IOException {
+            initialize("testN3.txt");
+            execute();
+            save("matches_testN3.txt");
     }
     public static void initialize(String st) throws IOException{
+        //reading input from file
         Scanner inFile1 = new Scanner(new File(st)).useDelimiter(",");
         List<String> tokens = new ArrayList<String>();
         while (inFile1.hasNext()) {
             tokens.add(inFile1.nextLine());
           }
+        //storing number n
         int n = Integer.parseInt(tokens.get(0));
         N = n;
         employersindices = new int[n];
@@ -26,27 +34,34 @@ public class MatchingClass{
         employers = new String[n];
         students = new String[n];
         Sue = new Stack<>();
+        //loop 1: pushing unmatched employers to Sue
         for(int i=0;i<n;i++){
           Sue.push(i);
         }
+          //loop 2: setting all employers and students to -1
         for(int i=0;i<n;i++){
           employersindices[i]=studentsindices[i]=-1;
         }
+        //loop 3: filling list of employers with their names
         int count1 = 0;
         for(int i=1;i<n+1;i++){
           employers[count1] = tokens.get(i);
           count1++;
         }
+        //loop 4: filling list of students with their names
         int count2 = 0;
         for(int i=n+1;i<2*n+1;i++){
           students[count2] = tokens.get(i);
           count2++;
         }
+        //loop 5: setting up the priority queue
         PQ = new ArrayList<>();
         for(int i=2*n+1;i<tokens.size();i++){
           int y = 0;
+          //replacing commas with spaces for easier traversal
           String mystring = tokens.get(i).replace(","," ").replace(" ","");
           HeapPriorityQueue<Integer,Integer> h = new HeapPriorityQueue<Integer,Integer>();
+          //loop 6: filling up PQ by skipping the second  element of each pair since we don't need it
           for(int x=0;x<n;x++){
               int a=Character.getNumericValue(mystring.charAt(y++));
               y++;
@@ -56,10 +71,12 @@ public class MatchingClass{
         }
         A = new int[n][n];
         int v = 0;
+        //loop 7: setting up the matrix A
         for(int i=2*n+1;i<tokens.size();i++){
           int y = 0;
           String mystring = tokens.get(i).replace(","," ").replace(" ","");
           int[] sublist = new int[n];
+          //loop 6: filling up A by skipping the first element of each pair since we don't need it
           for(int x=0;x<n;x++){
               y++;
               int a=Character.getNumericValue(mystring.charAt(y++));
@@ -69,7 +86,8 @@ public class MatchingClass{
             v++;
         }
       }
-      public static HashMap<Integer, Integer> execute(){
+      public static HashMap<Integer, Integer> execute()throws IOException{
+        //Gale-Shapley algorithm
           while(!Sue.isEmpty()){
             int e = Sue.pop();
             int s = PQ.get(e).removeMin().getValue();
@@ -88,10 +106,25 @@ public class MatchingClass{
               Sue.push(e);
             }
         }
-        HashMap<Integer, Integer> map = new HashMap<>();
+        //return hash map that stores the indice of the employer as key and the indice of the student matched to that employer as a value
+        map = new HashMap<>();
         for(int i=0;i<N;i++){
-          map.put(employersindices[i],i);
+          map.put(i,employersindices[i]);
         }
         return map;
+      }
+      public static void save(String filename)throws IOException{
+        String output = "";
+        int temp = 0;
+        //loop 8: extracting names of students and employers from the names arrays using keys and values stored in the HashMap
+        //setting up the ouput string to write to the output file
+        for(int i=0;i<N;i++){
+          int value = map.get(i);
+          output+="Match "+temp+": "+employers[i]+"-"+students[value]+"\n";
+          temp++;
+        }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        writer.write(output);
+        writer.close();
       }
 }
